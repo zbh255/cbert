962e217082f21e8ec80065f7a6f8d89c8ae1d6a0
zbh255 <565584326@qq.com>
2021-12-26 17:01:30 +0800

    move folder

--- protocol/handle.go
+++ protocol/handle.go
@@ -2,7 +2,9 @@ package protocol
 
 import "io"
 
-func DecodeHandshakeRequest(io io.Reader) (*HandshakeRequest, error) {}
+func DecodeHandshakeRequest(io io.Reader) (*HandshakeRequest, error) {
+
+}
 
 func EncodeHandshakeResponse(rep *HandshakeResponse) ([]byte, error) {}
 
