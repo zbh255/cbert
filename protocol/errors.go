73b49fecc2fae8d503d2e69038be718b744e4068
zbh255 <565584326@qq.com>
2021-12-27 15:52:12 +0800

    update protocol errors

--- protocol/errors.go
+++ protocol/errors.go
@@ -4,4 +4,5 @@ import "errors"
 
 var (
 	ErrProtocolFormatFailed = errors.New("error: protocol format failed")
+	ErrWriteBytesNoEqual    = errors.New("writer to connection byte number not equal")
 )
