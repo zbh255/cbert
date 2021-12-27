7dce41e921d5fbd109ced6835d362991f272aa27
zbh255 <565584326@qq.com>
2021-12-27 15:51:57 +0800

    update protocol encode test

--- protocol/decodeAndEncode_test.go
+++ protocol/decodeAndEncode_test.go
@@ -21,14 +21,14 @@ func TestHandshake(t *testing.T) {
 	requestBytes := append([]byte{
 		VERSION,
 		0x00,
-	},uuidBytes...)
+	}, uuidBytes...)
 	reader := bytes.NewReader(requestBytes)
-	request,err := DecodeHandshakeRequest(reader)
+	request, err := DecodeHandshakeRequest(reader)
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	if !bytes.Equal(request.UuidV4[:32],uuidBytes) {
+	if !bytes.Equal(request.UuidV4[:32], uuidBytes) {
 		t.Error(errors.New("the uuid not equal"))
 	}
 	t.Log(request)
@@ -46,7 +46,7 @@ func TestMessage(t *testing.T) {
 		VERSION,
 		CMD_GET_SIGNLE_DATA,
 		byte(len(fileName)),
-	},fileName...)
+	}, fileName...)
 	reader := bytes.NewReader(requestBytes)
 	request, err := DecodeMessageRequest(reader)
 	if err != nil {
@@ -54,4 +54,22 @@ func TestMessage(t *testing.T) {
 		return
 	}
 	t.Log(request)
+	response := new(MessageResponse)
+	response.Version = VERSION
+	response.Reserved = 0x00
+	response.Reply = REPLY_NOT_FILE
+	response.FileNameLength = request.FileNameLength
+	response.FileName = request.FileName
+	responseBytes := []byte{
+		VERSION,
+		0x00,
+		REPLY_NOT_FILE,
+		27,
+		47, 115, 115, 109, 45, 115, 116, 117, 100, 105, 111, 47, 101, 120, 99, 101, 112, 116, 105, 111, 110,
+		115, 46, 106, 115, 111, 110,
+	}
+	if !bytes.Equal(responseBytes, EncodeMessageResponse(response)) {
+		t.Error(errors.New("message response encode is not equal"))
+		return
+	}
 }
