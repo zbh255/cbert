aa1e4753e180b4ed1486d7a768fd2ec16cdda6c0
zbh255 <565584326@qq.com>
2021-12-28 21:17:34 +0800

    update encode and decode function give client use

--- protocol/handle.go
+++ protocol/handle.go
@@ -19,12 +19,40 @@ func DecodeHandshakeRequest(reader io.Reader) (*HandshakeRequest, error) {
 	return request, nil
 }
 
+// suport client
+func EncodeHandshakeRequest(request *HandshakeRequest) []byte {
+	if request == nil {
+		return nil
+	}
+	buffer := make([]byte,0,34)
+	buffer = append(buffer,request.Version)
+	buffer = append(buffer,request.Reserved)
+	buffer = append(buffer,request.UuidV4[:]...)
+	return buffer
+}
+
 func EncodeHandshakeResponse(rep *HandshakeResponse) []byte {
 	buffer := make([]byte, 0, 3)
 	buffer = append(buffer, rep.Version)
 	buffer = append(buffer, rep.Reserved)
 	buffer = append(buffer, rep.Reply)
 	return buffer
+}
+
+func DecodeHandshakeResponse(reader io.Reader) (*HandshakeResponse, error) {
+	buffer := make([]byte,3)
+	n,err := reader.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
+	if n != len(buffer) {
+		return nil, ErrReaderBytesNoEqual
+	}
+	return &HandshakeResponse{
+		Version:  buffer[0],
+		Reserved: buffer[1],
+		Reply:    buffer[2],
+	}, err
 }
 
 func DecodeMessageRequest(reader io.Reader) (*MessageRequest, error) {
@@ -53,6 +81,16 @@ func DecodeMessageRequest(reader io.Reader) (*MessageRequest, error) {
 	return request, err
 }
 
+func EncodeMessageRequest(request *MessageRequest) []byte {
+	buf := []byte{
+		request.Version,
+		request.Command,
+		request.FileNameLength,
+	}
+	buf = append(buf, request.FileName...)
+	return buf
+}
+
 func EncodeMessageResponse(response *MessageResponse) []byte {
 	buf := make([]byte, 0, 32)
 	buf = append(buf, response.Version)
@@ -60,4 +98,31 @@ func EncodeMessageResponse(response *MessageResponse) []byte {
 	buf = append(buf, response.Reply)
 	buf = append(buf, append([]byte{response.FileNameLength}, response.FileName...)...)
 	return buf
+}
+
+func DecodeMessageResponse(reader io.Reader) (*MessageResponse, error) {
+	buffer := make([]byte,4)
+	n, err := reader.Read(buffer)
+	if err != nil {
+		return nil,err
+	}
+	if n != len(buffer) {
+		return nil,ErrReaderBytesNoEqual
+	}
+	rep := &MessageResponse{
+		Version:        buffer[0],
+		Reserved:       buffer[1],
+		Reply:          buffer[2],
+		FileNameLength: buffer[3],
+		FileName:       nil,
+	}
+	rep.FileName = make([]byte,rep.FileNameLength)
+	n, err = reader.Read(rep.FileName)
+	if err != nil {
+		return nil, err
+	}
+	if n != int(rep.FileNameLength) {
+		return nil,ErrReaderBytesNoEqual
+	}
+	return rep,nil
 }
