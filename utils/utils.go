3fb51cd0a9a3df14eb3e9c71cd248c82bf037579
zbh255 <565584326@qq.com>
2021-12-28 21:10:02 +0800

    add read utils

---
+++ utils/utils.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"io"
+	"reflect"
+	"unsafe"
+)
+
+const (
+	BUFFER_SIZE int = 512
+)
+
+// link : https://github.com/zbh255/ss5-simple
+func ReadAll(reader io.Reader) ([]byte,error) {
+	readN := 0
+	buf := make([]byte, BUFFER_SIZE)
+	n, err := reader.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+	for n == BUFFER_SIZE {
+		readN += n
+		buf = append(buf, make([]byte, BUFFER_SIZE)...)
+		n, err = reader.Read(buf[readN : readN+BUFFER_SIZE])
+		if err == io.EOF {
+			n = 0
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+	readN += n
+
+	sh := (*reflect.SliceHeader)(unsafe.Pointer(&buf))
+	sh.Len = readN
+	return *(*[]byte)(unsafe.Pointer(sh)), nil
+}
