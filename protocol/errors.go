dd67c61090d7c41fc44fd1aba4ecf3471ab95c63
zbh255 <565584326@qq.com>
2021-12-28 21:14:10 +0800

    defined new errors

--- protocol/errors.go
+++ protocol/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrProtocolFormatFailed = errors.New("error: protocol format failed")
 	ErrWriteBytesNoEqual    = errors.New("writer to connection byte number not equal")
+	ErrReaderBytesNoEqual = errors.New("from connection read byte number not equal")
 )
